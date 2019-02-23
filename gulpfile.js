1cad9780c0320947de2659b34c8a08f4fe3e9285
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2019-02-22 23:34:59 -0600

    committing unsaved misc changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,29 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
 
 //Create a static server
-gulp.task('serve', ['sass'], function() {
-    browserSync.init({
-      browser: "google chrome",
-      server: {
-          baseDir: "./src/",
-      }
-    });
+gulp.task("serve", ["sass"], function() {
+	browserSync.init({
+		browser: "google chrome",
+		server: {
+			baseDir: "./src/",
+		}
+	});
 
-    gulp.watch("./src/public/sass/*.scss", ['sass']).on('change', browserSync.reload);
-    gulp.watch("./src/*.html").on('change', browserSync.reload);
+	// Watch files for changes and reload the browser
+	gulp.watch("./src/public/sass/*.scss", ["sass"]).on("change", browserSync.reload);
+	gulp.watch("./src/public/js/*.js").on("change", browserSync.reload);
+	gulp.watch("./src/*.html").on("change", browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("./src/public/sass/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("./src/public/css"))
-        .pipe(browserSync.stream());
+gulp.task("sass", function() {
+	return gulp.src("./src/public/sass/*.scss")
+		.pipe(sass())
+		.pipe(gulp.dest("./src/public/css"))
+		.pipe(browserSync.stream());
 });
 
-gulp.task('default', ['serve']);
+// Default task to run on 'npm start'
+gulp.task("default", ["serve"]);
