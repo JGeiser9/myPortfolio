4b02a1479358955176d7ec188f8935303414a0f1
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2019-03-19 20:37:02 -0500

    deployment file restructure

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,78 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const del = require("del");
+const uglify = require("gulp-uglify-es").default;
+const htmlmin = require("gulp-htmlmin");
+const imagemin = require("gulp-imagemin");
+const pipeline = require("readable-stream").pipeline;
 const browserSync = require("browser-sync").create();
 
 //Create a static server
-gulp.task("serve", ["sass"], function() {
+gulp.task("serve", function() {
 	browserSync.init({
 		browser: "google chrome",
 		server: {
-			baseDir: "./src/",
+			baseDir: "./build/",
 		}
 	});
-
-	// Watch files for changes and reload the browser
-	gulp.watch("./src/public/sass/*.scss", ["sass"]).on("change", browserSync.reload);
-	gulp.watch("./src/public/js/*.js").on("change", browserSync.reload);
-	gulp.watch("./src/*.html").on("change", browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task("sass", function() {
-	return gulp.src("./src/public/sass/*.scss")
-		.pipe(sass())
-		.pipe(gulp.dest("./src/public/css"))
-		.pipe(browserSync.stream());
+	return pipeline(
+		gulp.src("./src/sass/*.scss"),
+		// Compile sass to css and minify
+		sass({outputStyle: "compressed"}),
+		gulp.dest("./build/css"),
+		browserSync.stream()
+	);
+});
+
+// Compile JS files into build folder
+gulp.task("js", function() {
+	return pipeline(
+		gulp.src("./src/js/*.js"),
+		// Compress JS files
+		uglify(),
+		gulp.dest("./build/js")
+	);
+});
+
+// Compile HTML files into build folder
+gulp.task("html", function() {
+	return pipeline(
+		gulp.src("./src/**/*.html"),
+		// Minify HTML files
+		htmlmin({collapseWhiteSpace: true}),
+		gulp.dest("./build/")
+	);
+});
+
+// Compile IMG files into build folder
+gulp.task("img", function() {
+	return pipeline(
+		gulp.src("./src/images/*"),
+		// Minify images
+		imagemin(),
+		gulp.dest("./build/images")
+	);
+});
+
+// Watch for changes in files / live reload
+gulp.task("watch", function() {
+	// Watch files for changes and reload the browser
+	gulp.watch("./src/sass/*.scss", gulp.series("sass")).on("change", browserSync.reload);
+	gulp.watch("./src/js/*.js", gulp.series("js")).on("change", browserSync.reload);
+	gulp.watch("./src/**/*.html", gulp.series("html")).on("change", browserSync.reload);
+	gulp.watch("./src/images/*", gulp.series("img")).on("change", browserSync.reload);
+});
+
+gulp.task("clean", function() {
+	// Delete old files in build folder every fresh build.
+	return del(["./build"]);
 });
 
 // Default task to run on 'npm start'
-gulp.task("default", ["serve"]);
+gulp.task("build", gulp.parallel("sass", "js", "html", "img"));
+gulp.task("prod", gulp.series("clean", "build"));
+gulp.task("default", gulp.series("clean", "build", gulp.parallel("watch", "serve")));
